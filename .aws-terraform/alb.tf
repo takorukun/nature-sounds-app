fe7defd5f13e5b0eef32065ca20a4f1811803b84
takorukun <hyogokota@gmail.com>
2023-08-06 16:41:13 +0900

    albを作成

---
+++ .aws-terraform/alb.tf
@@ -0,0 +1,91 @@
+resource "aws_security_group" "alb_sg" {
+  name   = "alb_sg"
+  vpc_id = aws_vpc.main_app_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb_sg"
+  }
+}
+
+resource "aws_lb" "app_alb" {
+  name               = "app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_web_subnet1.id, aws_subnet.public_web_subnet2.id]
+
+  tags = {
+    Name = "app-alb"
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name     = "target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main_app_vpc.id
+  target_type = "ip"
+}
+
+resource "aws_lb_listener" "app_alb_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      status_code  = "200"
+      message_body = "ok"
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "app_alb_listener_rule" {
+  listener_arn = aws_lb_listener.app_alb_listener.arn
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = "${aws_lb.app_alb.arn}"
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = "${aws_acm_certificate_validation.cert_validation.certificate_arn}"
+
+  default_action {
+    type = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}"
+  }
+}
