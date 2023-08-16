ec167746279310b20295d27e0aa38bdfc680ad51
takorukun <hyogokota@gmail.com>
2023-08-16 18:09:55 +0900

    ネットワークバインドをtcpで記載

--- config/puma.rb
+++ config/puma.rb
@@ -4,6 +4,8 @@ port ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
+bind "tcp://0.0.0.0:3000"
+
 app_root = File.expand_path("../..", __FILE__)
 
 stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
