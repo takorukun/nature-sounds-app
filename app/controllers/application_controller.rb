481e8d454b5eed3c2289b31d1303115e6b295113
takorukun <hyogokota@gmail.com>
2023-08-17 13:15:48 +0900

    ヘルスチェック用のコントローラーを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::Base
   def index
     render html: '<h1>Welcome to My App</h1>'.html_safe
   end
-  
-  def health
-    render plain: 'OK', status: :ok
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  get '/health', to: 'health_checks#index'
   root 'application#index'
-  get '/health', to: 'application#health'
 end
