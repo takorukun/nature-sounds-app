019ed53410f362b42fe13a30303c0d897ebd06e8
takorukun <hyogokota@gmail.com>
2023-08-17 13:12:25 +0900

    環境設定を本番に変更

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 port ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
 plugin :tmp_restart
 
 bind "tcp://0.0.0.0:3000"
