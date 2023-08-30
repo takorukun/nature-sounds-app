d51fe997dfc3e93004f8cde217a61986b438606b
takorukun <hyogokota@gmail.com>
2023-08-30 21:50:21 +0900

    topページを大方形成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   get '/health', to: 'health_checks#index'
-  root 'application#index'
+  get '/top_page', to: 'top_page#index'
+  root to: 'top_page#index'
 end
