19b86c3ec701544124b4a0f6ba4c660e16fdc0e5
takorukun <hyogokota@gmail.com>
2023-09-02 20:18:31 +0900

    deviseの初期設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def random_background_image
+    backgrounds = Dir.glob("app/assets/images/bg-images_1280/*").map do |path|
+      File.basename(path)
+    end
+    "bg-images_1280/#{backgrounds.sample}"
+  end
 end
