1a3251447a0d9862ff8a30db31d713dc0e152510
takorukun <hyogokota@gmail.com>
2023-09-23 18:58:07 +0900

    Rubocopで修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
   def db_test
-    begin
-      result = ActiveRecord::Base.connection.execute("SELECT 1 AS test")
-      render json: { status: 'success', result: result.first }
-    rescue => e
-      Rails.logger.error("DB Test failed: #{e.message}")
-      render json: { status: 'error', message: 'Database connection test failed.' }, status: 500
-    end
-  end 
+    result = ActiveRecord::Base.connection.execute("SELECT 1 AS test")
+    render json: { status: 'success', result: result.first }
+  rescue => e
+    Rails.logger.error("DB Test failed: #{e.message}")
+    render json: { status: 'error', message: 'Database connection test failed.' }, status: 500
+  end
 end
