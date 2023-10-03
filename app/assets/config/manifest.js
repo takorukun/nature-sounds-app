85ab1515d6d8e277dadc60d6c3891fe1ee51f810
takorukun <hyogokota@gmail.com>
2023-10-03 15:23:15 +0900

    sccsロードエラーに対応&manifest.jsを修正

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link_tree ../builds
-//= link_tree ../stylesheets
+//= link_tree ../../javascript/controllers .js
+//= link_tree ../builds .css
+//= link_tree ../stylesheets .css
