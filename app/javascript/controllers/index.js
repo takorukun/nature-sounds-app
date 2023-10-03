6edb695f34099085fa476ab3801f5061ae4c02ef
takorukun <hyogokota@gmail.com>
2023-10-03 19:45:07 +0900

    scssエラーのため再度gemをインストールしjsファイルの参照エラーのため修正

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,4 @@
 // Import and register all your controllers from the importmap under controllers/*
-import "application.js";
-import "hello_controller.js";
 import { application } from "controllers/application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
