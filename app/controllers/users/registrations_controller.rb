3f12713d2b97f7122a5d963a84056970ad013667
takorukun <hyogokota@gmail.com>
2023-10-09 22:50:19 +0900

    ゲストユーザーの情報編集を規制し、一般ユーザーの画像更新の際空白でも以前設定していたものを保持するよう設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+  before_action :ensure_normal_user, only: %i[update destroy]
 
   def new
     super
@@ -15,7 +16,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def update
+    if params[:user][:avatar].blank?
+      params[:user].delete(:avatar)
+    end
     super
+  end
+
+  def ensure_normal_user
+    if resource.email == 'guest@example.com'
+      if action_name == 'destroy'
+        redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
+      elsif action_name == 'update'
+        redirect_to user_path(current_user), alert: 'ゲストユーザーの再設定はできません。'
+      end
+    end
   end
 
   def destroy
