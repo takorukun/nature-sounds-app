d8b921eac2c00d68e91b4255afea75cbc28609b7
takorukun <hyogokota@gmail.com>
2023-10-15 12:43:21 +0900

    Rubocopで修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,23 +32,23 @@ end
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :rack_test
-    
+
     config.before(:suite) do
       DatabaseCleaner.clean_with(:truncation)
     end
-  
+
     config.before(:each) do
       DatabaseCleaner.strategy = :transaction
     end
-  
+
     config.before(:each, type: :feature) do
       DatabaseCleaner.strategy = :truncation
     end
-  
+
     config.before(:each) do
       DatabaseCleaner.start
     end
-  
+
     config.after(:each) do
       DatabaseCleaner.clean
     end
