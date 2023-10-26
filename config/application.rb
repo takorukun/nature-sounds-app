ff48f215abde2916532bc27c9aae2541f49259a2
takorukun <hyogokota@gmail.com>
2023-10-26 23:29:43 +0900

    デコレーターを設定

--- config/application.rb
+++ config/application.rb
@@ -8,5 +8,8 @@ module Myapp
   class Application < Rails::Application
     config.load_defaults 7.0
     config.i18n.default_locale = :ja
+    config.to_prepare do
+      Dir[Rails.root.join('app', 'models', '*_decorator.rb')].each { |f| require_dependency f }
+    end
   end
 end
