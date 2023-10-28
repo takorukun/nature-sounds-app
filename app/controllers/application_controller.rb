b12fce200ebabd3b72898eaef804a031ce7e2b50
takorukun <hyogokota@gmail.com>
2023-10-28 18:03:36 +0900

    DBステータステストを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
 class ApplicationController < ActionController::Base
-  def db_test
-    result = ActiveRecord::Base.connection.execute("SELECT 1 AS test")
-    render json: { status: 'success', result: result.first }
-  rescue => e
-    Rails.logger.error("DB Test failed: #{e.message}")
-    Rails.logger.error(e.backtrace.join("\n"))
-    render json: { status: 'error', message: 'Database connection test failed.' }, status: 500
-  end
 end
