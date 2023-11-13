4a635b55894733e9342a0e41ad6b86e2192ce6b2
takorukun <hyogokota@gmail.com>
2023-11-13 20:56:01 +0900

    ロジックをヘルパーに移動

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
 module ApplicationHelper
+  def user_avatar_url(user)
+    return nil unless user.avatar.attached?
+
+    key = user.avatar.blob.key
+    s3 = Aws::S3::Resource.new(
+      region: ENV['AWS_REGION'] || 'ap-northeast-1',
+      credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY'], ENV['AWS_SECRET_ACCESS_KEY'])
+    )
+    signer = Aws::S3::Presigner.new(client: s3.client)
+    signer.presigned_url(:get_object, bucket: 'main-bucket-takorukun', key: key, expires_in: 60)
+  end
+
   def random_background_image
     backgrounds = Dir.glob("app/assets/images/bg-images_1280/*").map do |path|
       File.basename(path)
