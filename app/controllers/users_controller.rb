82c3d3fe33af00cc70f9d080c6fad79fd3a506c7
takorukun <hyogokota@gmail.com>
2023-12-07 22:13:57 +0900

    feat: 瞑想の頻度を表示する機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,7 @@ class UsersController < ApplicationController
     @calendar_events = @meditations.map do |m|
       { id: m.id, duration: m.duration, date: m.date, notes: m.notes, created_at: m.created_at, video_id: m.video_id }
     end
+    @meditations_this_week = UserService.count_meditations_this_week(@calendar_events)
+    @reclaiming_the_habits = UserService.count_reclaiming_the_habit_of_meditation(@calendar_events)
   end
 end
---
+++ app/services/user_service.rb
@@ -0,0 +1,31 @@
+class UserService
+  def self.count_meditations_this_week(events)
+    start_of_week = Date.today.beginning_of_week(:monday)
+    end_of_week = start_of_week + 6.days
+
+    events.count { |event| event[:date] >= start_of_week && event[:date] <= end_of_week }
+  end
+
+  def self.count_reclaiming_the_habit_of_meditation(events)
+    sorted_events = events.sort_by { |event| event[:date] }
+    count = 0
+    streak = 0
+
+    sorted_events.each_with_index do |event, index|
+      if index == 0
+        streak = 1
+        next
+      end
+
+      if event[:date] == sorted_events[index - 1][:date] + 1.day
+        streak += 1
+      else
+        count += 1 if streak >= 2
+        streak = 1
+      end
+    end
+
+    count += 1 if streak >= 2
+    count
+  end
+end
