3262ecf06c38f4d238d1fccf4eb072e47aae793a
takorukun <hyogokota@gmail.com>
2023-12-13 23:28:04 +0900

    feat: 2番目の瞑想スケジュールを達成後、3番目以降のスケジュールを表示するロジックを追加

--- app/models/meditation.rb
+++ app/models/meditation.rb
@@ -3,6 +3,10 @@ class Meditation < ApplicationRecord
   validates_presence_of :date
   validates :notes, length: { maximum: 500 }, allow_blank: true
 
+  def self.sessions_for_user(user, start_date, end_date)
+    where(user_id: user, date: start_date..end_date)
+  end
+
   belongs_to :user
   belongs_to :video
 end
